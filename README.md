ce985b08d638371a8f744a9d93613ecaf5239cfa
Rainer Simon <rainer@rainersimon.io>
2025-02-20 15:06:05 +0100

    Initial import

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # plugin-segment-anything
-A smart polygon selection tool based on SegmentAnything 2.
+
+A fully browser-based smart polygon selection tool for Annotorious based on the [sam2-hiera-tiny](https://huggingface.co/g-ronimo/sam2-tiny) SegmentAnything model. The basic approach is inspired by this [blog post](https://medium.com/@geronimo7/in-browser-image-segmentation-with-segment-anything-model-2-c72680170d92) and [demo code](https://github.com/geronimi73/next-sam). 
+
+
---
+++ vite.config.js
@@ -0,0 +1,39 @@
+import { defineConfig } from 'vite';
+import path from 'path';
+import { svelte } from '@sveltejs/vite-plugin-svelte';
+import { sveltePreprocess } from 'svelte-preprocess';
+import dts from 'vite-plugin-dts';
+
+export default defineConfig({
+  plugins: [
+    svelte({ preprocess: sveltePreprocess() }),
+    dts({
+      insertTypesEntry: true,
+      include: ['./src/'],
+      entryRoot: './src'
+    })
+  ],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src')
+    }
+  },
+  server: {
+    open: '/test/index.html'
+  },
+  build: {
+    sourcemap: true,
+    lib: {
+      entry: './src/index.ts',
+      name: 'AnnotoriousSmartTools',
+      formats: ['es', 'umd'],
+      fileName: (format) => 
+        format === 'umd' ? `annotorious-plugin-sam.js` : `annotorious-plugin-sam.es.js` 
+    },
+    rollupOptions: {
+      output: {
+        assetFileNames: 'annotorious-plugin-sam.[ext]'
+      }
+    }
+  }
+});
